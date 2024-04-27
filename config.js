f4bdfb77e959247779bbf9c0bfb00470e6468b8e
LORDLONELYDEVIL <lordlonelydevil001@gmail.com>
2024-04-27 16:59:17 +0530

    bm backend

---
+++ config.js
@@ -0,0 +1,2 @@
+export const PORT=5555;
+export const mongoDBURL='mongodb+srv://lordlonelydevil:lordlonelydevil001@cluster0.iwmppm4.mongodb.net/cluster0?retryWrites=true&w=majority&appName=Cluster0';
---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from './models/bookModels.js';
+import bookRoute from "./routes/bookRoute.js";
+import cors from "cors";
+
+
+const app = express();
+app.use(express.json());//middleware for parsing request to body
+
+//middleware for handling cors policy
+//option1:Allow all origins with default of cors(*)
+app.use(cors());
+//option2 :allow custom origins
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ['Content-Type'],
+// }));
+
+app.get("/", (request, response) => {
+    //console.log(request);
+    return response.status(200).send("Welcome");
+})
+
+app.use("/books", bookRoute);
+
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("connected");
+        app.listen(PORT, () => {
+            console.log(`app is listening to port ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+
+    })
