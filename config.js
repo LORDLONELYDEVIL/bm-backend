5ba0695048916c8d2d6fc1b67d460794ea7476ef
LORD LONELYDEVIL <147601125+LORDLONELYDEVIL@users.noreply.github.com>
2024-09-28 08:53:29 +0530

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-export const PORT=5555;
-export const mongoDBURL='mongodb+srv://lordlonelydevil:lordlonelydevil001@cluster0.iwmppm4.mongodb.net/cluster0?retryWrites=true&w=majority&appName=Cluster0';
